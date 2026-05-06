c69ba0f9282d2cbc8e477edf1af9136b164d3a24
Gitesh Bondre <126845755+bgitesh@users.noreply.github.com>
2026-05-06 07:24:53 +0000

    Organize EC2 configuration into its own folder

---
+++ EC2-Instance-launching/main.tf
@@ -0,0 +1,44 @@
+#VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "my_subnet" {
+   vpc_id = aws_vpc.my_vpc.id
+   cidr_block = var.subnet_cidr
+}
+
+resource "aws_security_group" "my_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+  
+  ingress {
+   from_port = 22
+   to_port = 22
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+   most_recent = true
+   owners = ["099720109477"]
+
+   filter {
+      
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+   }  
+}
+
+# EC2
+resource "aws_instance" "my_ec2" {
+   ami = data.aws_ami.ubuntu.id
+   instance_type = var.instance_type
+
+   subnet_id = aws_subnet.my_subnet.id
+   vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+   tags = {
+      Name = "Terraform instance"
+   }
+}
---
+++ EC2-Instance-launching/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+    default = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+     default = "17.68.0.0/16"
+}
+
+variable "subnet_cidr" {
+     default = "17.68.0.0/24"
+}
+
+variable "instance_type" {
+   default = "t3.micro"
+}
+
